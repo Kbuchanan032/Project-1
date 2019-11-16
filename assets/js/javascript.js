325137704d1b5fad9a8c2f38fce5f3ecb8bd51e1
Serguei Rodriguez <sjosevich@gmail.com>
2019-11-16 10:56:57 -0700

    Adding Ajax in order tp pull info from google API

---
+++ assets/js/javascript.js
@@ -0,0 +1,30 @@
+var topics = ["Restaurants", "Bars"]
+
+var totalInfo = 10;
+//var location=-33.8670522,151.1957362;
+// var radius=500;
+// var types=food;
+// var name=harbour;
+var apiKey = 'AIzaSyAirI4H5b9p94B9ZkgEX0a_r_NIOt5RktY';
+
+
+$(document).ready(function(){
+    
+
+function getInfo(){
+    $.ajax({
+        url: 'http://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key='+apiKey,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+    })
+}
+
+$("#im-bored").click(function(){
+    //console.log('click')
+    getInfo()
+})
+
+
+
+})
