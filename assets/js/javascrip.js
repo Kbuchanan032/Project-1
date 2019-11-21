a06a122ab63b5745d9cc45ed29fa2529186039ce
Serguei Rodriguez <sjosevich@gmail.com>
2019-11-21 13:37:21 -0700

    Now our App has only one javascript file. geoloaction.html and index.html looks for javascrip.js only

--- assets/js/javascrip.js
+++ assets/js/javascrip.js
@@ -1,6 +1,4 @@
 var map, infoWindow;
-
-
 function initMap() {
   
   
@@ -16,18 +14,19 @@ function initMap() {
      
       map = new google.maps.Map(document.getElementById('mapNearPlaces'), {
       center: userCity ,
-      zoom: 15
+      zoom: 13
     });
 
     var request = {
     location: userCity,
-    radius: '5000',
+    radius: '2000',
     types: ['restaurant', 'Bars', 'night clubs'],
+    
     };
 
     infoWindow = new google.maps.InfoWindow;
       console.log(userCity)
-      service = new google.maps.places.PlacesService(map);
+     service = new google.maps.places.PlacesService(map);
       service.nearbySearch(request, callback);
 
       // infoWindow.setPosition(pos);
@@ -55,20 +54,31 @@ function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
       for (var i = 0; i < results.length; i++) {
           var place = results[i];
-          console.log(place);
-          createMarker(results[i]);
+          var request1 = { placeId: place.place_id }  // Here I created my plceId opject
+          service.getDetails(request1, function(place, status) {  // here I'm passing the place ID in order to gte the place details
+              console.log(status)
+              if (status == google.maps.places.PlacesServiceStatus.OK) {
+                console.log(place)
+                var localPlaces = $('#show_details');
+                var divImage = $("#show_images")
+                var placeImage = $('<img/>')
+                placeImage.attr('src', place.photos[0].getUrl({maxWidth: '200', maxHeight: '200'}));
+                placeImage.appendTo(divImage)
+                var placesName = $("<div>" + localPlaces + "</div><br>")
+                localPlaces.append("<br>" + place.name + "<br>" + place.vicinity + "<br>" + place.formatted_phone_number + "<br>")
+                var marker = new google.maps.Marker({
+                  map: map,
+                  position: place.geometry.location                      
+                });
+                google.maps.event.addListener(marker, 'click', function() {
+                  infoWindow.setContent(place.name);
+                  infoWindow.open(map, this);
+                });
+              }
+          });
+          
+          
       }
   }
 }  
 
-function createMarker(place) {
-  var marker = new google.maps.Marker({
-      map: map,
-      position: place.geometry.location
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-      infowindow.setContent(place.name);
-      infowindow.open(map, this);
-  });
-}
